11a85f2a15609ec1e6d317425f980b611a81c33c
Modern Tech <mordenfarm1677@gmail.com>
2025-12-08 17:21:18 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ import AnalyticsDashboard from './features/admin/AnalyticsDashboard';
 
 export default function App() {
   useEffect(() => {
-    seedDatabase();
+    //seedDatabase();
   }, []);
 
   return (
